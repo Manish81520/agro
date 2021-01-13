99d8d0d8a3b279eebf42a90360dc7aa02bb12954
Manish81520 <55319766+Manish81520@users.noreply.github.com>
2021-01-13 22:52:29 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="page-footer black">
+          <div class="footer-copyright">
+            <div class="container">
+            <a class="grey-text text-lighten-4 right black" href="#!">More Links</a>
+            </div>
+          </div>
+</footer>
+</body>
---
+++ register.php
@@ -0,0 +1,85 @@
+<?php 
+	ob_start();
+	
+	$email=$name=$password=$role='';
+	$errors=array('email'=>'','name'=>'','password'=>'','role'=>'');
+	if (isset($_POST['submit'])) {
+		if (empty($_POST['email'])) {
+			$errors['email'] ='email is required';
+		}
+		else{
+			$email=$_POST['email'];
+			if (!filter_var($email,FILTER_VALIDATE_EMAIL)) {
+				$errors['email']='enter valid email address';
+			}
+		}
+		if (empty($_POST['name'])) {
+			$errors['name']='username is required';
+		}
+		else{
+			$name=$_POST['name'];
+		}
+		if (empty($_POST['password'])) {
+			$errors['password']='password is required';
+		}
+		else{
+			$password=$_POST['password'];
+		}
+		if (empty($_POST['role'])) {
+			$errors['role']='select one option';
+		}
+		else{
+			$role=$_POST['role'];
+		}
+		if (array_filter($errors)) {
+		
+		}
+		else{
+			
+		}
+	}
+	else
+	{
+		echo '';
+	}
+ ?>
+
+<!DOCTYPE html>
+<html>
+<?php include('templates/header.php'); ?>
+
+<main>
+	<div class="container" style="width:300px;padding-top: 50px;">
+		<form method="POST">
+			<div class="input-field col s12">
+    			<select name="role">
+      			<option value="" disabled selected>Choose your option</option>
+      			<option value="farmer">FARMER</option>
+      			<option value="buyer">BUYER</option>
+    			</select>
+    			<label>Register as</label>
+  			</div>
+  			<div class="red-text left-align" style="width: 200px;"><?php echo $errors['role']; ?></div>
+			<label>Your email</label>
+			<input type="text" name="email" value="<?php echo htmlspecialchars($email);?>">
+			<div class="red-text left-align" style="width: 200px;"><?php echo $errors['email']; ?></div>
+			<label>User Name</label>
+			<input type="text" name="name" value="<?php echo htmlspecialchars($name);?>">
+			<div class="red-text left-align" style="width: 200px;"><?php echo $errors['name']; ?></div>
+			<label>password</label>
+			<input type="text" name="password" value="<?php echo htmlspecialchars($password);?>">
+			<div class="red-text left-align" style="width: 200px;"><?php echo $errors['password']; ?></div>
+			<div class="center">
+				<input type="submit" name="submit" value="submit" class="btn z-depth-0">
+			</div>
+		</form>
+	</div>
+	<script type="text/javascript">
+		$(document).ready(function(){
+    	$('select').formSelect();
+  	});
+	</script>
+</main>
+
+<?php include('templates/footer.php'); ?>
+</html>
